dfb917be0e795b13c1a8fbd7cc0d4e2ee1b51912
Karina Kuss <karina.kuss00@gmail.com>
2023-05-06 16:32:07 +0200

    added a route for creating a new friend

--- server.js
+++ server.js
@@ -23,21 +23,39 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} | ${delta}ms`);
 })
 
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  const { name } = req.body
+  if(!name) return res.status(400).json({ message: "Invalid name." })
+
+  const newFriend = {
+    id: friends.length,
+    name
+  };
+
+  friends.push(newFriend);
+
+  res.json({
+    friend: newFriend
+  });
+});
+
 app.get('/friends', (req, res) => {
-  res.json(friends)
-})
+  res.json(friends);
+});
 
 app.get('/friends/:id', (req, res) => {
   const friend = friends[req.params.id * 1];
-  if(!friend) return res.status(404).json({ message: "Friend not found."})
+  if(!friend) return res.status(404).json({ message: "Friend not found."});
   
-  res.status(200).json(friend)
-})
+  res.status(200).json(friend);
+});
 
 app.get('/', (req, res) => {
-  res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`)
+  console.log(`Server listening on port ${PORT}...`);
 });
